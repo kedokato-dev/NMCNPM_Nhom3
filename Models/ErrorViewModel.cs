f3acde8c837b94b97f5af26ababc9c9138317161
Trần Anh Quân <thocodeanhquan@gmail.com>
2025-02-21 23:00:20 +0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NMCNPM_Nhom3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
