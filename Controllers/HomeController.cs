c4d66d7cf9b67f263449cf44240c4f4b53abbdab
phucvip123 <113783304+phucvip123@users.noreply.github.com>
2025-02-23 23:21:42 +0700

    Add Popup Login, Register

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,18 +1,23 @@
 ﻿using System.Diagnostics;
 using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using NMCNPM_Nhom3.Models;
+using NMCNPM_Nhom3.Models.Entities;
 
 namespace NMCNPM_Nhom3.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly NmcnpmContext _context;
+        public HomeController(ILogger<HomeController> logger,NmcnpmContext context)
         {
             _logger = logger;
+            _context = context;
         }
         //[Authorize(Roles = "Admin")]
         public IActionResult Index()
@@ -24,6 +29,34 @@ namespace NMCNPM_Nhom3.Controllers
         public IActionResult Privacy()
         {
             return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login(string username, string password)
+        {
+            var user = _context.TblAccounts.Include(u => u.SPermissionsNavigation).FirstOrDefault(u => u.SPhoneNumber == username && u.SPassword == password);
+            if (user == null)
+            {
+                ViewBag.Message = "Invalid login credentials.";
+                return Json(new { success = false, message = "Tên đăng nhập hoặc mật khẩu không đúng!" });
+            }
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.SAccountName),
+                new Claim("Phone", user.SPhoneNumber),
+                new Claim(ClaimTypes.Role,  user.SPermissionsNavigation?.SPermissionn??"ban")
+            };
+            var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+            var principal = new ClaimsPrincipal(identity);
+
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
+
+            return Json(new { success = true });
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Login");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
