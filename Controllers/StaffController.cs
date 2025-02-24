458e81b01051211b7b58d78978450b7a41b536be
Trần Anh Quân <thocodeanhquan@gmail.com>
2025-02-24 08:14:48 +0700

    update staff

--- Controllers/StaffController.cs
+++ Controllers/StaffController.cs
@@ -16,12 +16,10 @@ namespace NMCNPM_Nhom3.Controllers
 
         public IActionResult Index()
         {
-            if (IsLoggedIn())
-            {
-                var staff = _context.TblAccounts.ToList();
-                return View(staff);
-            }
-            return RedirectToAction("Login", "Login");
+            var staff = _context.TblAccounts
+                                .Where(a => a.SPermissions == 1 )
+                                .ToList();
+            return View(staff);
         }
 
         public IActionResult Create()
@@ -30,36 +28,34 @@ namespace NMCNPM_Nhom3.Controllers
         }
 
         [HttpPost]
-        [ValidateAntiForgeryToken]
         public IActionResult Create(TblAccount account)
         {
-            if (!IsLoggedIn())
-                return RedirectToAction("Login", "Login");
-
             if (!ModelState.IsValid)
             {
-                var errors = ModelState.Values.SelectMany(v => v.Errors);
+                ViewBag.SuccessMessage = "Thêm tài khoản thất bại";
+                ViewBag.RedirectUrl = Url.Action("Create", "Staff");
                 return View(account);
             }
 
-            try
+            if (_context.TblAccounts.Any(a => a.SPhoneNumber == account.SPhoneNumber) ||
+                _context.TblAccounts.Any(a => a.SUserIdentification == account.SUserIdentification))
             {
-                account.SPassword = BCrypt.Net.BCrypt.HashPassword(account.SPassword);
-
-                _context.TblAccounts.Add(account);
-                _context.SaveChanges();
-                return Json(new { message = "Thêm tài khoản thành công, tài khoản: " + account.SAccountName, redirectUrl = "/Staff/Index" });
-               
-
-
-            }
-            catch (Exception ex)
-            {
-                //Console.WriteLine("Lỗi khi thêm tài khoản: ");
-                //ModelState.AddModelError("", "Có lỗi xảy ra khi thêm tài khoản." + ex.ToString());
+                ViewBag.SuccessMessage = "Tài khoản này đã tồn tại";
+                ViewBag.RedirectUrl = Url.Action("Create", "Staff");
                 return View(account);
             }
+
+            _context.TblAccounts.Add(account); // Thêm tài khoản vào DB
+            _context.SaveChanges(); // Lưu thay đổi
+
+            ViewBag.SuccessMessage = $"Thêm tài khoản thành công: {account.SAccountName}";
+            ViewBag.RedirectUrl = Url.Action("Index", "Staff"); // Chuyển hướng sau khi thêm thành công
+            return View(account);
         }
+
+
+
+
 
         public IActionResult Edit(int id)
         {
@@ -72,40 +68,59 @@ namespace NMCNPM_Nhom3.Controllers
             return View(account);
         }
 
-
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Edit(TblAccount account)
+        public async Task<IActionResult> Edit(int id, TblAccount account)
         {
-            if (ModelState.IsValid)
+            if (!ModelState.IsValid)
             {
-                var existingAccount = _context.TblAccounts.Find(account.PkIdUser);
-                if (existingAccount == null)
-                {
-                    return NotFound();
-                }
-
-                // Cập nhật thông tin tài khoản
-                existingAccount.SAccountName = account.SAccountName;
-                existingAccount.SPhoneNumber = account.SPhoneNumber;
-                existingAccount.DDate = account.DDate;
-                existingAccount.SPermissions = account.SPermissions;
-                existingAccount.SUserIdentification = account.SUserIdentification;
-
-                // Nếu có mật khẩu mới thì mã hóa và lưu, nếu không thì giữ nguyên mật khẩu cũ
-                if (!string.IsNullOrEmpty(account.SPassword))
-                {
-                    existingAccount.SPassword = BCrypt.Net.BCrypt.HashPassword(account.SPassword);
-                }
-
-                _context.SaveChanges();
-                return RedirectToAction("Index");
+                return View(account);
             }
 
-            return View(account);
+            var existingAccount = await _context.TblAccounts.FindAsync(id);
+            if (existingAccount == null)
+            {
+                return NotFound();
+            }
+
+            // Kiểm tra trùng số điện thoại & CCCD (trừ chính nó)
+            bool isPhoneNumberExist = _context.TblAccounts.Any(a => a.SPhoneNumber == account.SPhoneNumber && a.PkIdUser != id);
+            bool isCCCDExist = _context.TblAccounts.Any(a => a.SUserIdentification == account.SUserIdentification && a.PkIdUser != id);
+
+            if (isPhoneNumberExist)
+            {
+                ModelState.AddModelError("SPhoneNumber", "Số điện thoại đã tồn tại.");
+                return View(account);
+            }
+            if (isCCCDExist)
+            {
+                ModelState.AddModelError("SUserIdentification", "Số CCCD đã tồn tại.");
+                return View(account);
+            }
+
+            // Cập nhật thông tin thủ công
+            existingAccount.SAccountName = account.SAccountName;
+            existingAccount.SPhoneNumber = account.SPhoneNumber;
+            existingAccount.DDate = account.DDate;
+            existingAccount.SPermissions = account.SPermissions;
+            existingAccount.SUserIdentification = account.SUserIdentification;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+                TempData["SuccessMessage"] = "Cập nhật tài khoản thành công!";
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "Có lỗi xảy ra khi cập nhật: " + ex.Message);
+                return View(account);
+            }
         }
 
-       public IActionResult Delete(int id)
+
+
+        public IActionResult Delete(int id)
         {
             var account = _context.TblAccounts.Find(id);
             if (account == null)
@@ -121,9 +136,6 @@ namespace NMCNPM_Nhom3.Controllers
         }
 
 
-        private bool IsLoggedIn()
-        {
-            return !string.IsNullOrEmpty(HttpContext.Session.GetString("AccountId"));
-        }
+
     }
 }
